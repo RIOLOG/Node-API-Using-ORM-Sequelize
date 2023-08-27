8bc80342e47aab8bdc2cf587fe1db352cb6ac53a
Ankit Singh <84015430+RIOLOG@users.noreply.github.com>
2023-08-27 16:55:01 +0530

    Update customer.controller.js

--- customer.controller.js
+++ customer.controller.js
@@ -6,7 +6,7 @@ function createCustomer(req, res)
 {
     if (!req.body.name || !req.body.email || !req.body.age)
     {
-        return res.status(400).send({message:"some filed are missing"});
+        return res.status(400).send({message:"some field are missing"});
     }
     const customerObject = {
         name:req.body.name,
